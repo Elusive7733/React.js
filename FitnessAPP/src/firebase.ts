f77f1ffce0c92e991fec1d8d5f03eb225bf004b0
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-12-09 22:53:13 +0500

    Added Firebase

---
+++ FitnessAPP/src/firebase.ts
@@ -0,0 +1,20 @@
+// import firebase from "firebase/app";
+// import "firebase/database";
+import { initializeApp } from "firebase/app"; // no compat for new SDK
+import { getDatabase, ref } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWP4MEpLjRIpphJzT92UAlJgZKUqzesRE",
+  authDomain: "fine-fitness-83c83.firebaseapp.com",
+  databaseURL:
+    "https://fine-fitness-83c83-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fine-fitness-83c83",
+  storageBucket: "fine-fitness-83c83.appspot.com",
+  messagingSenderId: "408468786875",
+  appId: "1:408468786875:web:aa7f2ca262267f1325e39e",
+  measurementId: "${config.measurementId}",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
