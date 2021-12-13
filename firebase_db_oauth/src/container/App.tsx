e42f91a927be945353551b8e69807b30f0ca4b6f
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-12-13 13:22:53 +0500

    Something working :+1:

--- firebase_db_oauth/src/container/App.tsx
+++ firebase_db_oauth/src/container/App.tsx
@@ -1,7 +1,9 @@
 import React, { Fragment, useState } from "react";
 
 import Home from "../pages/Home/Home";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 //todo: set authentication
 const App: React.FC = () => {
@@ -18,6 +20,11 @@ const App: React.FC = () => {
           <Link to="/register">register</Link>
         </Fragment>
       )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </Fragment>
   );
 };
--- firebase_db_oauth/src/index.tsx
+++ firebase_db_oauth/src/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./container/App";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
